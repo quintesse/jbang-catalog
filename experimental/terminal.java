a691925ce499a0bff213d6e1059334f77da48c43
Tako Schotanus <tako@codejive.org>
2023-03-08 00:20:33 +0100

    chore: new terminal test and user can now select test to run

--- experimental/terminal.java
+++ experimental/terminal.java
@@ -4,6 +4,7 @@
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +18,15 @@ import org.jline.utils.AttributedString;
 import org.jline.utils.Display;
 import org.jline.utils.InfoCmp.Capability;
 
+@Command(name="terminal", subcommands = {info.class, display.class, box.class, scroll.class, swap.class})
 class terminal {
     public static void main(String... args) {
-        int exitCode = new CommandLine(new box()).execute(args);
+        int exitCode = new CommandLine(new terminal()).execute(args);
         System.exit(exitCode);
     }
 }
 
-@Command
+@Command(name="info")
 class info implements Callable<Integer> {
 
     @Override
@@ -40,15 +42,11 @@ class info implements Callable<Integer> {
         System.out.println("   hasFocusSupport: " + terminal.hasFocusSupport());
         System.out.println("   hasMouseSupport: " + terminal.hasMouseSupport());
         System.out.println("   attributes: " + terminal.getAttributes());
-
-        terminal.enterRawMode();
-        terminal.reader().read();
-
         return 0;
     }
 }
 
-@Command
+@Command(name="display")
 class display implements Callable<Integer> {
  
     @Override
@@ -73,21 +71,21 @@ class display implements Callable<Integer> {
                 lines2.add(new AttributedString(String.format("%03d: %s", i, "Chaine de test...")));
             }
 
-            // Display with tempo
             display.update(lines1, 0);
-            Thread.sleep(3000);
+            terminal.reader().read();
 
             display.update(lines2, 0);
-            Thread.sleep(3000);
+            terminal.reader().read();
 
             terminal.setAttributes(savedAttributes);
             terminal.puts(Capability.exit_ca_mode);
+            terminal.writer().flush();
             return 0;
         }
     }    
 }
 
-@Command
+@Command(name="box")
 class box implements Callable<Integer> {
  
     @Override
@@ -110,16 +108,10 @@ class box implements Callable<Integer> {
             display.resize(h, w);
 
             // Build Strings to displayed
-            List<AttributedString> lines = new ArrayList<>();
-            lines.add(new AttributedString(String.format("+%s+", "-".repeat(w - 2))));
-            for (int i = 0; i < h-2; i++) {
-                lines.add(new AttributedString(String.format("|%s|", " ".repeat(w - 2))));
-            }
-            lines.add(new AttributedString(String.format("+%s+", "-".repeat(w - 2))));
+            List<AttributedString> lines = createBox(w, h);
 
-            // Display with tempo
             display.update(lines, 0);
-            Thread.sleep(3000);
+            terminal.reader().read();
 
             terminal.setAttributes(savedAttributes);
             terminal.puts(Capability.exit_ca_mode);
@@ -127,20 +119,78 @@ class box implements Callable<Integer> {
 
             return 0;
         }
+    }
+
+    public static List<AttributedString> createBox(int w, int h) {
+        List<AttributedString> lines = new ArrayList<>();
+        lines.add(new AttributedString(String.format("+%s+", "-".repeat(w - 2))));
+        for (int i = 0; i < h-2; i++) {
+            lines.add(new AttributedString(String.format("|%s|", " ".repeat(w - 2))));
+        }
+        lines.add(new AttributedString(String.format("+%s+", "-".repeat(w - 2))));
+        return lines;
     }    
 }
 
-@Command
+@Command(name="scroll")
 class scroll implements Callable<Integer> {
+ 
+    @Parameters(index = "0", arity = "1", description = "Size of scroll region in lines")
+    int lines;
+
+    @Override
+    public Integer call() throws Exception {
+        try (Terminal terminal = TerminalBuilder.builder().build()) {
+            if (lines > 0 && lines < terminal.getHeight()) {
+                terminal.enterRawMode();
+            } else {
+                lines = terminal.getHeight();
+            }
+            terminal.puts(Capability.change_scroll_region, 0, lines-1);
+            terminal.flush();
+            return 0;
+        }
+    }    
+}
+
+@Command(name="swap")
+class swap implements Callable<Integer> {
  
     @Override
     public Integer call() throws Exception {
 
         try (Terminal terminal = TerminalBuilder.builder().build()) {
             Attributes savedAttributes = terminal.enterRawMode();
-            terminal.puts(Capability.change_scroll_region, 0, 5);
-            terminal.flush();
+            Display display = new Display(terminal, false);
+            Size size = new Size();
+            size.copy(terminal.getSize());
+
+            int w = size.getColumns();
+            int h = size.getRows();
+
+            display.resize(5, w);
+            display.update(box.createBox(w, 5), 0);
+            terminal.reader().read();
+
+            display.resize(10, w);
+            display.update(box.createBox(w, 10), 0);
+            terminal.reader().read();
+
+            terminal.puts(Capability.enter_ca_mode);
+            terminal.writer().flush();
+
+            display.clear();
+            display.reset();
+            display.resize(h, w);
+            display.update(box.createBox(w, h), 0);
+            terminal.reader().read();
+
+            terminal.puts(Capability.exit_ca_mode);
+            terminal.writer().flush();
+
+            terminal.setAttributes(savedAttributes);
+
             return 0;
         }
-    }    
+    }
 }
